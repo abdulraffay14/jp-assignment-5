3538f4c6175e1e8a03bc8d4ad80e584df924fe8f
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-31 18:30:44 +0500

    Assignment 5 question 5

---
+++ q5.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, String> contact = {
+    'name': 'Nuhammad Raffay',
+    'phone': '03219547825',
+    'city': 'Karachi',
+    'code': 'PSID123'
+  };
+
+  List<String> keysList = contact.keys.toList();
+  keysList.retainWhere((key) => key.length == 4);
+
+  print(keysList);
+}
