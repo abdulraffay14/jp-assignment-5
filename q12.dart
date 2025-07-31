5ab3f49a2cda084ffb61661e79d8b9e2690fc5f0
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-31 20:22:17 +0500

    Assignment 5 question 12

---
+++ q12.dart
@@ -0,0 +1,8 @@
+void main() {
+  List<String> originalList = ['apple', 'banana', 'cherry', 'date'];
+
+  List<String> reversedList = originalList.reversed.toList();
+
+  print('Original List: $originalList');
+  print('Reversed List: $reversedList');
+}
