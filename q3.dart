96c9080f33f29449d8926aa684b9a25e4ab492c0
abdulraffay14 <abdulraffay9292@gmail.com>
2025-07-31 18:02:58 +0500

    Assignemnt 5 question 3

---
+++ q3.dart
@@ -0,0 +1,21 @@
+void main() {
+  List<String> days = [
+    'Monday','Tuesday','Wednesday',
+    'Thursday','Friday',
+    'Saturday','Sunday'
+    ];
+print (days);
+
+
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+  print(days.removeLast());
+
+  print(days);
+
+  print(days.isEmpty);
+}
